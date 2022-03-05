78af72df21a21c2a01a378ad5d250e865c453b89
Janson Xavier <jansonxavier15@gmail.com>
2022-03-05 17:05:51 -0500

    application add and get features

--- src/server/controllers/roleController.js
+++ src/server/controllers/roleController.js
@@ -2,86 +2,88 @@
 
 const db = require('../models/dbConnection');
 
-const roleController = {}
+const roleController = {};
 
-// Get role from a user
+// Get a role
 roleController.getRole = (req, res, next) => {
-  const { _id } = req.body;
+  const { id } = req.params;
   const queryString = 'SELECT * FROM roles WHERE _id = $1';
 
-  db.query(queryString, [_id])
-    .then(data => {
+  db.query(queryString, [id])
+    .then((data) => {
       res.locals.role = data.rows[0];
       return next();
     })
-    .catch(err => next({
+    .catch((err) => next({
       log: `Error in roleController.getRole: ${err}`,
       message: { err: 'Error getting Role' },
     }));
 };
 
+// Get all roles
 roleController.getAllRoles = (req, res, next) => {
   const queryString = 'SELECT * FROM roles';
 
   db.query(queryString)
-    .then(data => {
+    .then((data) => {
       res.locals.allRoles = data.rows;
       return next();
     })
-    .catch(err => next({
+    .catch((err) => next({
       log: `Error in roleController.getAllRoles: ${err}`,
       message: { err: 'Error getting roles' },
     }));
 };
 
-// Add a new role to a user
+// Add a new role
 roleController.addRole = (req, res, next) => {
   const { title, department } = req.body;
-  const queryString = 'INSERT INTO roles (title, department) VALUES ($1 $2)';
+  const queryString = 'INSERT INTO roles (title, department) VALUES ($1, $2) RETURNING *';
 
   const variables = [title, department];
 
   db.query(queryString, variables)
-    .then(data => {
+    .then((data) => {
       res.locals.role = data.rows[0];
       return next();
     })
-    .catch(err => next({
+    .catch((err) => next({
       log: `Error in roleController.addRole: ${err}`,
       message: { err: 'Error adding Role' },
     }));
 };
 
-// Update the user's role
+// Update the role
 roleController.updateRole = (req, res, next) => {
-  const { _id, title, department } = req.body;
+  const { id } = req.params;
+  const { title, department } = req.body;
 
-  const queryString = 'UPDATE roles SET tite=$2, department=$3 WHERE _id=$1';
+  const queryString = 'UPDATE roles SET title=$2, department=$3 WHERE _id=$1 RETURNING *';
 
-  const variables = [_id, title, department];
+  const variables = [id, title, department];
 
   db.query(queryString, variables)
-    .then(data => {
+    .then((data) => {
       res.locals.role = data.rows[0];
       return next();
     })
-    .catch(err => next({
+    .catch((err) => next({
       log: `Error in roleController.updateRole: ${err}`,
       message: { err: 'Error updating Role' },
     }));
 };
 
-// Delete a user's role
+// Delete a role
 roleController.deleteRole = (req, res, next) => {
-  const { _id } = req.body;
-  const queryString = 'DELETE FROM roles WHERE _id=$1';
+  const { id } = req.params;
+  const queryString = 'DELETE FROM roles WHERE _id=$1 RETURNING *';
 
-  db.query(queryString, [_id])
-    .then(data => {
+  db.query(queryString, [id])
+    .then((data) => {
       res.locals.role = data.rows[0];
       return next();
     })
-    .catch(err => next({
+    .catch((err) => next({
       log: `Error in roleController.deleteRole: ${err}`,
       message: { err: 'Error deleting Role' },
     }));

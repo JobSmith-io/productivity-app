f264cfa8e928a6543ea6164e40bfcb7e8ede1727
Janson Xavier <jansonxavier15@gmail.com>
2022-03-04 19:51:17 -0500

    server boilerplate

---
+++ src/server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const apiRouter = require('./routes/api');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api', apiRouter);
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' }, 
+  };
+
+  const errObj = { ...defaultErr, ...err};
+  console.log(errObj.log);
+  res.status(errObj.status).send(errObj.message);
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`)
+});
